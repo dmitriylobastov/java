1a54e07fff325b6db113c854843764e9d860e84b
dmitriylobastov <119615202+dmitriylobastov@users.noreply.github.com>
2023-08-21 21:35:22 +0300

    OK

--- src/main/java/org/example/Start.java
+++ src/main/java/org/example/Start.java
@@ -1,15 +1,13 @@
 package org.example;
 
+import java.sql.SQLOutput;
 import java.util.Arrays;
-
-/*
-Переворот массива
-*/
 
 public class Start {
     public static void main(String[] args) {
-            long a = 2;
+            long a = 4;
             System.out.println(ninthDegree(a));
+        System.out.println("OK");
         }
         public static long ninthDegree(long a) {
             return a*a;
