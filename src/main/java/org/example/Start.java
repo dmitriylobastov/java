11ff7ca8b7a0c65dac1e608b8e13f1e3b5733771
dmitriylobastov <119615202+dmitriylobastov@users.noreply.github.com>
2023-08-21 21:30:17 +0300

    Add files via upload
    Start java

---
+++ src/main/java/org/example/Start.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Arrays;
+
+/*
+Переворот массива
+*/
+
+public class Start {
+    public static void main(String[] args) {
+            long a = 2;
+            System.out.println(ninthDegree(a));
+        }
+        public static long ninthDegree(long a) {
+            return a*a;
+        }
+    }
